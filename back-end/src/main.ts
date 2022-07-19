6faf7f41ba6890dce383202131eaece831edcb16
barimehdi77 <mbari@student.1337.ma>
2022-07-19 13:00:59 -0400

    change the cros

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -7,7 +7,7 @@ import * as cookieParser from 'cookie-parser'
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
   // app.use(
   //   session({
